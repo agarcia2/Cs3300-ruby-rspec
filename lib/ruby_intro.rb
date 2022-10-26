be36b4c1be888ec5ce2e0566f9ec42ba35ae948d
root <agarcia2@uccs.edu>
2022-10-26 02:09:30 +0000

    Commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,78 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each{|x|sum+=x}
+  return sum;
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length ==0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  end
+  arr=arr.sort
+  return arr[-1] + arr[-2]
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
-end
+#def sum_to_n? arr, n
+  #(arr.empty? &&n.zero?) || arr.permutation(2).any? {|a,b| a + b ==n}
+#end
+def sum_to_n? arr,n
+  return false if arr.empty? || n.zero?
+  return true if arr.empty? && n.zero?
+  arr.combination(2).any?{|a,b| a + b ==n}
+end  
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return true if /^[^aeiou\W]/i === s 
+  #return true if /^[^aeiou\W]/i === s 
+  #consonant = /^[^aeiou\d\W_]/i
+  #return true if consonant.match(s)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  /^[01]+$/.match(s) && s.to_i(2) % 4 == 0 ? true : false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_reader :isbn
+  attr_accessor :price
+
+  def Getisbn
+    @isbn
+  end
+
+  def isbn=(isbn)
+    @isbn=isbn
+  end
+
+  def Getprice
+    @price
+  end
+
+  def price=(price)
+    @price = Float(price)
+  end
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty?||price<=0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    return sprintf("$%2.2f", price)
+  end
+
+
 end
